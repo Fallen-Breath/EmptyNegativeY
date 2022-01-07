6717535fdef1e49a8f5328c206b31004674efb20
Fallen_Breath <yqx791125833@gmail.com>
2022-01-07 07:50:25 -0600

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+## EmptyNegativeY
---
+++ src/main/java/me/fallenbreath/emptynegativey/mixins/ChunkHeightAndBiomeFixMixin.java
@@ -0,0 +1,38 @@
+package me.fallenbreath.emptynegativey.mixins;
+
+import net.minecraft.datafixer.fix.ChunkHeightAndBiomeFix;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+@Mixin(ChunkHeightAndBiomeFix.class)
+public abstract class ChunkHeightAndBiomeFixMixin
+{
+	@ModifyArg(
+			method = "fixLevel",
+			slice = @Slice(
+					from = @At(
+							value = "INVOKE",
+							target = "Lnet/minecraft/datafixer/fix/ProtoChunkTickListFix$class_6741;method_39265(III)Lcom/mojang/serialization/Dynamic;",
+							ordinal = 0
+					),
+					to = @At(
+							value = "INVOKE",
+							target = "Ljava/util/BitSet;set(I)V",
+							ordinal = 0
+					)
+			),
+			at = @At(
+					value = "INVOKE",
+					target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z"
+			),
+			allow = 2,
+			require = 2,
+			index = 0
+	)
+	private static Object ItIsAlwaysAir(Object value)
+	{
+		return "minecraft:air";
+	}
+}
